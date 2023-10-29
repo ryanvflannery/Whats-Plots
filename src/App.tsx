0d850e09b5c1f36c77c2de4a3d5b6f19488a30e5
Aidan <abayercalvert@gmail.com>
2023-10-28 17:48:49 -0700

    components in app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState, ChangeEvent, SetStateAction } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
@@ -9,61 +10,25 @@ import {
 } from "convex/react";
 import { api } from "../convex/_generated/api";
 import { useAuth } from "@clerk/clerk-react";
-import { useEffect, useState, ChangeEvent, SetStateAction } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
 
 export default function App() {
   const { isLoaded, userId, sessionId, getToken } = useAuth();
-  const [groupName, setGroupName] = useState("");
-  const allGroups = useQuery(api.myFunctions.getAllGroupsForUser) || [];
-  const [emailAddressAddUser, setEmailAddressAddUser] = useState("");
-  const [emailAddressRemoveUser, setEmailAddressRemoveUser] = useState("");
-
-  const createGroup = useMutation(api.myFunctions.createNewGroup);
-  const addMemberToGroup = useMutation(api.myFunctions.addMemberToGroup);
-  const removeMemberFromGroup = useMutation(
-    api.myFunctions.removeUserFromGroup
-  );
-
-  const handleGroupNameChange = (
-    event: ChangeEvent<HTMLInputElement>
-  ): void => {
-    setGroupName(event.target.value);
-  };
-
-  const handleEmailAddChange = (e: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    // Assuming this function is handling the change of the email input
-    setEmailAddressAddUser(e.target.value);
-  };
-
-  const handleEmailRemoveChange = (e: {
-    target: { value: SetStateAction<string> };
-  }) => {
-    // Assuming this function is handling the change of the email input
-    setEmailAddressRemoveUser(e.target.value);
-  };
-
-  const handleCreateGroup = () => {
-    void createGroup({
-      name: groupName,
-      groupMembers: [],
-    });
-  };
-
-  const handleAddMemberToGroup = () => {
-    void addMemberToGroup({
-      groupID: "457dhkw7rbdmabkgdbjnxyhp9k510f8",
-      userID: emailAddressAddUser,
-    });
-  };
-
-  const handleRemoveMemberGroup = () => {
-    void removeMemberFromGroup({
-      groupID: "457dhkw7rbdmabkgdbjnxyhp9k510f8",
-      userID: emailAddressRemoveUser,
-    });
-  };
 
   return (
     <main className="container max-w-2xl flex flex-col gap-8">
@@ -75,36 +40,9 @@ export default function App() {
         <div>
           Hello, {userId} your current active session is {sessionId}
         </div>
-        <Input
-          value={groupName}
-          onChange={handleGroupNameChange}
-          placeholder="Group Name"
-        ></Input>
-
-        <Button onClick={handleCreateGroup}>create new group</Button>
-        <h1>All Groups</h1>
-        <ul>
-          {allGroups.map((group, index) => (
-            <li key={index}>
-              {/* Display the group details here */}
-              Group Name: {group.name}
-            </li>
-          ))}
-        </ul>
-        <Input
-          value={emailAddressAddUser}
-          onChange={handleEmailAddChange}
-          placeholder="Enter Email Address To Add To Group"
-        ></Input>
-        <Button onClick={handleAddMemberToGroup}>Add member to group</Button>
-        <Input
-          value={emailAddressRemoveUser}
-          onChange={handleEmailRemoveChange}
-          placeholder="Enter Email Address To Remove From Group"
-        ></Input>
-        <Button onClick={handleRemoveMemberGroup}>
-          Remove Member From Group
-        </Button>
+        <AddGroup></AddGroup>
+        <RemoveGroupMember></RemoveGroupMember>
+        <AddGroupMember></AddGroupMember>
       </Authenticated>
 
       <Unauthenticated>
@@ -115,6 +53,168 @@ export default function App() {
         </div>
       </Unauthenticated>
     </main>
+  );
+}
+
+function RemoveGroupMember() {
+  const [emailAddressRemoveUser, setEmailAddressRemoveUser] = useState("");
+  const removeMemberFromGroup = useMutation(
+    api.myFunctions.removeUserFromGroup
+  );
+
+  const handleEmailRemoveChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    // Assuming this function is handling the change of the email input
+    setEmailAddressRemoveUser(e.target.value);
+  };
+
+  const handleRemoveMemberGroup = () => {
+    void removeMemberFromGroup({
+      groupID: "457dhkw7rbdmabkgdbjnxyhp9k510f8",
+      userID: emailAddressRemoveUser,
+    });
+  };
+
+  return (
+    <>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Remove A Group Member</CardTitle>
+          <CardDescription>Input An Email Address To Remove</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email</Label>
+                <Input
+                  id="name"
+                  value={emailAddressRemoveUser}
+                  onChange={handleEmailRemoveChange}
+                  placeholder="Enter Email Address To Remove From Group"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button onClick={handleRemoveMemberGroup}>
+            Remove Member From Group
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
+
+function AddGroupMember() {
+  const [emailAddressAddUser, setEmailAddressAddUser] = useState("");
+
+  const addMemberToGroup = useMutation(api.myFunctions.addMemberToGroup);
+
+  const handleEmailAddChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    // Assuming this function is handling the change of the email input
+    setEmailAddressAddUser(e.target.value);
+  };
+
+  const handleAddMemberToGroup = () => {
+    void addMemberToGroup({
+      groupID: "457dhkw7rbdmabkgdbjnxyhp9k510f8",
+      userID: emailAddressAddUser,
+    });
+  };
+
+  return (
+    <>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Add A Group Member</CardTitle>
+          <CardDescription>Input An Email Address To Add</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  value={emailAddressAddUser}
+                  onChange={handleEmailAddChange}
+                  placeholder="Enter Email Address To Add To Group"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button onClick={handleAddMemberToGroup}>create new group</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
+
+function AddGroup() {
+  const [groupName, setGroupName] = useState("");
+  const allGroups = useQuery(api.myFunctions.getAllGroupsForUser) || [];
+  const createGroup = useMutation(api.myFunctions.createNewGroup);
+
+  const handleGroupNameChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setGroupName(event.target.value);
+  };
+
+  const handleCreateGroup = () => {
+    void createGroup({
+      name: groupName,
+      groupMembers: [],
+    });
+  };
+
+  return (
+    <>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Create A New Group</CardTitle>
+          <CardDescription>Make A New Group In One Click</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  value={groupName}
+                  onChange={handleGroupNameChange}
+                  placeholder="Group Name"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button onClick={handleCreateGroup}>create new group</Button>
+        </CardFooter>
+      </Card>
+
+      <h1>All Groups</h1>
+      <ul>
+        {allGroups.map((group, index) => (
+          <li key={index}>
+            {/* Display the group details here */}
+            Group Name: {group.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
